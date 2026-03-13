e5599ad0d71242c74200ebf77c7ed8722482b739
Ismael Fernández Méndez <ifm562@inlumine.ual.es>
2026-03-13 14:49:01 +0100

    feat: add script for building MVSIM refactor: move scripts into a dedicated script folder

---
+++ scripts/buildAndStartMVSIM.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Clone repository
+git clone https://github.com/MRPT/mvsim.git --recursive
+
+# Build
+colcon build
+
+# Update ROS environment
+source install/setup.bash
+
+# Load sim
+ros2 launch mvsim demo_jackal.launch.py
