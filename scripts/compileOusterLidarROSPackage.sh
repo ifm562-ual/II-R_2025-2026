bc5819b1cad17954ef235ae2c1c3d67b9706ec49
Ismael Fernández Méndez <ifm562@inlumine.ual.es>
2026-03-18 11:17:54 +0100

    fix: missing dependencies for ouster_ros and libzip for system container

--- scripts/compileOusterLidarROSPackage.sh
+++ scripts/compileOusterLidarROSPackage.sh
@@ -2,6 +2,10 @@
 cd ~/ros2_ws/src
 git clone -b humble-devel https://github.com/ouster-lidar/ouster-ros.git
 cd ..
-colcon build --packages-select ouster-ros
+# Make sure libzip is in system
+sudo apt update
+sudo apt install libzip-dev
+# Install all dependencies of that package
+colcon build --packages-up-to ouster_ros
 source install/setup.bash
 
