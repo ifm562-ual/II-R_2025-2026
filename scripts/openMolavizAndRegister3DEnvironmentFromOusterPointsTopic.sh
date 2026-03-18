2e8abbf8714633542f6e5cb4cd38deeb47b9f509
Ismael Fernández Méndez <ifm562@inlumine.ual.es>
2026-03-18 11:43:56 +0100

    fix: incorect new line in script, fix incorrect space between commands

--- scripts/openMolavizAndRegister3DEnvironmentFromOusterPointsTopic.sh
+++ scripts/openMolavizAndRegister3DEnvironmentFromOusterPointsTopic.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-ros2 launch mola_lidar_odometry ros2-lidar-odometry.launch.py \mola_deskew_method:=MotionCompensationMethod::IMU \lidar_topic_name:=/ouster/points imu_topic_name:=/ouster/imu
-mola_tf_base_link:=os_sensor
+ros2 launch mola_lidar_odometry ros2-lidar-odometry.launch.py \mola_deskew_method:=MotionCompensationMethod::IMU \lidar_topic_name:=/ouster/points imu_topic_name:=/ouster/imu mola_tf_base_link:=os_sensor
